f35c08ae246b6982538c515faaa2850c24ad945f
Gabriel Vasconcelos <gabrieltotal6@gmail.com>
2023-06-13 23:52:32 -0300

    config: adicionando a fonte roboto no projeto

--- src/components/commons/Layout.js
+++ src/components/commons/Layout.js
@@ -1,12 +1,18 @@
 import { Header } from "./Header"
+import { Roboto } from 'next/font/google'
+
+const roboto = Roboto({
+    subsets: ['latin'],
+    weight: ['100', '400', '700'],
+})
 
 export const Layout = ( {children}) => {
     return (
-        <>
+        <div className={roboto.className}>
             <Header />
 
             {children}
-        </>
+        </div>
         
     )
 }
