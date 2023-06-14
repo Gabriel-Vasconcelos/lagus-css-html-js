855efa9ab51bcf18535b7c5d7b4480ce5b80dd2f
Gabriel Vasconcelos <gabrieltotal6@gmail.com>
2023-06-14 01:38:45 -0300

    feat: adicionando o header da aplicação+

--- src/components/commons/Header.js
+++ src/components/commons/Header.js
@@ -2,22 +2,39 @@ import Link from "next/link"
 
 export const Header = () => {
    return (
-        <header>
+        <header className="bg-background-secondary flex justify-between items-center py-3 px-4">
             <button>
-                <img src="/IconMenu.svg" alt="icone de menu"/>
+                <img 
+                    src="/IconMenu.svg" 
+                    alt="icone de menu"
+                    width="20px"
+                />
             </button>
 
             <Link href="/">
-                <img src="/LogoLagus.svg" alt="logo de lagus_"/>
+                <img 
+                    src="/LogoLagus.svg" 
+                    alt="logo de lagus_"
+                    width="118px"
+                    height="48px"
+                />
             </Link>
             
-            <div>
+            <div className="flex gap-x-4">
                 <Link href="#">
-                    <img src="/IconPerson.svg" alt="icone de perfil"/>
+                    <img 
+                        src="/IconPerson.svg" 
+                        alt="icone de perfil"
+                        width="18px"
+                    />
                 </Link>
 
                 <Link href="/">
-                    <img src="/IconCart.svg" alt="icone de carro de compra" />
+                    <img 
+                        src="/IconCart.svg" 
+                        alt="icone de carro de compra" 
+                        width="24px"
+                    />
                 </Link>
             </div>
 
