17e12831ccc4bb0f7cb547b9a6c6bd19d3123bac
Gabriel Vasconcelos <gabrieltotal6@gmail.com>
2023-06-16 12:14:59 -0300

    feat: finalizando a responsividade da seção image list component

--- src/components/commons/ImageItem.js
+++ src/components/commons/ImageItem.js
@@ -1,16 +1,17 @@
 import { Text } from "./Text"
 
-export const ImageItem = ({titleImage, urlImage, altImage, styleCard}) => {
+export const ImageItem = ({titleImage, urlImage, altImage, styleCard, styleImage}) => {
     return (
-        <div className={`relative w-fit ${styleCard}`}> 
+        <div className={` relative w-fit ${styleCard} `}> 
             <h2 className="font-normal text-base absolute w-full bg-black bg-opacity-75 px-4 pt-[14px] pb-[13px] rounded-t-lg ">
                 {titleImage}
             </h2>
             <img 
                 src={urlImage} 
                 alt={altImage}
-                className="rounded-lg w-full object-cover"
+                className={`rounded-lg object-cover ${styleImage}`}
             />
         </div>
+
     )
 }
--- src/components/sections/SectionImageList.js
+++ src/components/sections/SectionImageList.js
@@ -8,12 +8,13 @@ export const SectionImageList = () => {
                 Image List Component
             </TitleComponent>
 
-            <div className="mt-8 lg:mt-12 grid grid-cols-1 lg:grid-cols-3 place-items-center gap-y-[14px] max-w-5xl mx-auto bg-red-500">
+            <div className="mt-8 lg:mt-12 grid grid-cols-1 lg:grid-cols-3 place-items-center gap-y-4 lg:gap-y-6 lg:gap-x-6 max-w-5xl mx-auto">
                 <ImageItem 
                     titleImage="Atom in the hand of a robot" 
                     urlImage="/images/PicAtom1.svg" 
                     altImage="imagem de um átomo pairando sobre uma mão robótica"
                     styleCard={'lg:col-span-1'}
+                    styleImage={'w-[400px] h-[400px] lg:h-[500px]'}
                 />
 
                 <ImageItem 
@@ -21,7 +22,8 @@ export const SectionImageList = () => {
                     urlImage="/images/PicManCode1.svg" 
                     altImage="imagem de um homem olhanado para um computador"
                     styleCard={'lg:col-span-2'}
-                    widthImage={''}
+                    styleImage={'w-[400px] h-[400px] lg:w-[700px] lg:h-[500px]'}
+
                 />
 
                 <ImageItem 
@@ -29,6 +31,7 @@ export const SectionImageList = () => {
                     urlImage="/images/PicWomanCode1.svg" 
                     altImage="imagem de uma mulher com fone de ouvido sorrindo"
                     styleCard={'lg:col-span-3'}
+                    styleImage={'w-[400px] lg:w-[1024px]'}
                 />
 
             </div>
